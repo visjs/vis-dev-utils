ba46072bf3013dfbfae5493b0afa8934021c081e
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-12-30 11:12:51 +0100

    feat: export header and Babel preset (#5)
    These two things are almost identical across our projects already and it
    makes a lot of sense to have them shared from a single source.
    
    Babel preset (TypeScript and CSS are optional):
    ```json
    {
      "exclude": ["**/node_modules/**"],
      "presets": [["vis-dev-utils/babel-preset", { "css": true, "ts": true }]]
    }
    ```
    
    Header (reads package.json):
    ```javascript
    import { generateHeader } from "vis-dev-utils";
    
    const banner = generateHeader();
    ```
    
    custom values can be supplied if necessary (tsdoc included):
    ```javascript
    const banner = generateHeader({ name: 'vis-timeline and vis-graph2d' });
    ```

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,78 @@
 import babel from "rollup-plugin-babel";
 import builtins from "builtin-modules";
 import commonjs from "rollup-plugin-commonjs";
-import genHeader from "./lib/header";
+import { generateHeader } from "./lib/header";
 import nodeResolve from "rollup-plugin-node-resolve";
 import packageJSON from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 import { string } from "rollup-plugin-string";
 
+const bannerModule = generateHeader();
 // Shebang is necessary to execute this as a command.
-const banner = "#!/usr/bin/env node\n\n" + genHeader("network");
+const bannerCommand = "#!/usr/bin/env node\n\n" + bannerModule;
 
 // Dependencies will be installed by npm.
 // Builtins are always available in Node.
-const external = [...Object.keys(packageJSON.dependencies), ...builtins];
+const external = [
+  ...new Set([
+    ...Object.keys(packageJSON.dependencies),
+    ...Object.keys(packageJSON.peerDependencies),
+    ...builtins
+  ])
+];
 
-export default ["generate-examples-index"].map(name => {
-  return {
-    input: `./src/${name}`,
-    output: {
-      banner,
-      file: `./bin/${name}.js`,
-      format: "cjs",
-      sourcemap: true
-    },
+const getPlugins = () => [
+  nodeResolve({
+    extensions: [".css", ".html", ".js", ".json", ".ts", ".txt"],
+    preferBuiltins: true
+  }),
+  string({
+    include: ["**/*.{css,html,txt}"]
+  }),
+  typescript({
+    objectHashIgnoreUnknownHack: true,
+    tsconfig: "tsconfig.json"
+  }),
+  commonjs(),
+  babel({
+    extensions: [".js", ".ts"],
+    runtimeHelpers: true
+  })
+];
+
+export default [
+  // JavaScript module exports.
+  {
+    input: `src/module`,
+    output: [
+      {
+        banner: bannerModule,
+        file: `dist/vis-dev-utils.cjs.js`,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        banner: bannerModule,
+        file: `dist/vis-dev-utils.esm.js`,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
     external,
-    plugins: [
-      nodeResolve({
-        extensions: [".css", ".html", ".js", ".json", ".ts", ".txt"],
-        preferBuiltins: true
-      }),
-      string({
-        include: ["**/*.{css,html,txt}"]
-      }),
-      typescript({
-        objectHashIgnoreUnknownHack: true,
-        tsconfig: "tsconfig.json"
-      }),
-      commonjs(),
-      babel({
-        extensions: [".js", ".ts"]
-      })
-    ]
-  };
-});
+    plugins: getPlugins()
+  },
+  // Node commands.
+  ...["generate-examples-index"].map(name => {
+    return {
+      input: `src/${name}`,
+      output: {
+        banner: bannerCommand,
+        file: `bin/${name}.js`,
+        format: "cjs",
+        sourcemap: true
+      },
+      external,
+      plugins: getPlugins()
+    };
+  })
+];
