cf4956fc04472963828e85dd9f6a905eeb54f356
Tomina <tomina@vycital.eu>
2023-03-31 20:54:14 +0200

    test(package): increase timeout

--- test/package.test.ts
+++ test/package.test.ts
@@ -4,6 +4,7 @@ import { inspectNpmPack } from "../lib/inspect-npm-pack";
 
 describe("package", function (): void {
   it("exported files", function (): void {
+    this.timeout(5000);
     snapshot(inspectNpmPack());
   });
 });
