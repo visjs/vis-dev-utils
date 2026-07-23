3c09e751cf27bbebee46bafcdf1018ecbaf49c68
Tomina <tomina@vycital.eu>
2026-07-23 18:24:07 +0200

    chore(lint): overhaul (ESLint → Oxlint, Prettier → Oxfmt, + autofix CI job)

---
+++ linting-and-formatting-ignore-patterns.ts
@@ -0,0 +1,14 @@
+export const ignorePatterns: string[] = [
+  "/*",
+
+  "!/*?.?*",
+  "!/.?*.?*",
+
+  "!/src",
+  "!/test",
+
+  "!/@types",
+  "!/babel-preset",
+  "!/eslint-shareable-config",
+  "!/public",
+];
